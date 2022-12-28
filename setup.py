1a445948c717cb52cbfefbd74203766cbfadcf26
David Nidever <dnidever@lsst.org>
2022-12-27 19:40:37 -0700

    setup update

--- setup.py
+++ setup.py
@@ -1,22 +1,15 @@
+from distutils.core import setup
+from distutils.sysconfig import get_python_lib
 import os
 import sys
-import shutil
-from setuptools import setup, find_packages, find_namespace_packages
-from setuptools.command.install import install
+import fileinput
+import platform
 import subprocess
+from setuptools.command.install import install
 
-def get_virtualenv_path():
-    """Used to work out path to install compiled binaries to."""
-    if hasattr(sys, 'real_prefix'):
-        return sys.prefix
-
-    if hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix:
-        return sys.prefix
-
-    if 'conda' in sys.prefix:
-        return sys.prefix
-
-    return None
+from platform import system as current_platform
+from shutil import copy, move, copytree, copyfile
+from glob import glob
 
 def get_bin_path():
     # Get environment bin/ directory
@@ -54,38 +47,158 @@ def get_bin_path():
         raise Exception('No bin/ directory found')
 
     return bindir
-    
-def compile_and_install_software():
-    """Used the subprocess module to compile/install the C software."""
-    src_path = './src/'
-    
-    # Install the software
-    print('Compiling the Fortran code')
-    ret = subprocess.run(['make'], cwd=src_path, shell=True)
 
+# We need to build MOOG and MOOGSILENT before they get moved to the scripts/
+# directory so that they can be moved into the $PATH
+if 'install' in sys.argv:
+
+    # Identify the platform
+    platform = current_platform()
+
+    # Check for platform first
+    if platform not in ('Darwin', 'Linux'):
+        sys.stderr.write("Platform '%s' not recognised!\n" % platform)
+        sys.exit()
+
+    # By default, we will use 32bit 
+    is_64bits = False
+
+    # Which system are we on?
+    if platform == 'Darwin':
+        run_make_files = ('Makefile.mac', 'Makefile.macsilent')
+        machine = 'mac'
+    elif platform == 'Linux':
+        machine = 'pcl'
+        is_64bits = sys.maxsize > 2**32
+        if is_64bits:
+            run_make_files = ('Makefile.rh64', 'Makefile.rh64silent')
+        else:
+            run_make_files = ('Makefile.rh', 'Makefile.rhsilent')
+
+    # Check for gfortran or g77
+    def system_call(command):
+        """ Perform a system call with a subprocess pipe """
+        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+        return process.communicate()[0]
+
+    # Look for g77 and gfortran
+    g77_exists = len(system_call("which g77")) > 0
+    gfortran_exists = len(system_call("which gfortran")) > 0
+
+    # If we have the choice, use gfortran
+    if gfortran_exists:
+        if is_64bits:
+            fortran_vars = "FC = gfortran -m64\nFFLAGS = -Wall -O4 -ffixed-line-length-72 -ff2c"
+        else:
+            fortran_vars = "FC = gfortran\nFFLAGS = -Wall -O4 -ffixed-line-length-72 -ff2c"
+    elif g77_exists:
+        if platform == 'Linux':
+            fortran_vars = 'FC = g77 -Wall'
+        else:
+            fortran_vars = 'FC = g77 -w'
+    else:
+        sys.stderr.write("Could not find g77 or gfortran on the system!\n")
+        sys.exit()
+
+    # Get our directories relative to the current path
+    repository_dir = os.path.dirname(os.path.realpath(__file__))
+    
+    # We need a moog data directory
+    data_dir = os.path.expanduser('~/.moog')
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+
+    # Copy files to data directory
+    src_dir = os.path.join(repository_dir, 'src')
+    data_files = glob('%s/*.dat' % src_dir)
+    [copy(data_file, '%s/%s' % (data_dir, os.path.basename(data_file), )) for data_file in data_files]
+    
+    aqlib = "AQLIB = %s" % os.path.join(repository_dir, 'lib/aqlib')
+    smlib = "SMLIB = %s" % os.path.join(repository_dir, 'lib/smlib')
+
+    configuration = "\n".join([fortran_vars, aqlib, smlib])
+
+    # Update the makefiles with the proper SMLIB and AQLIB
+    run_make_files = [os.path.join(repository_dir, 'src', filename) for filename in run_make_files]
+    hardcoded_moog_files = [os.path.join(repository_dir, 'src', filename) for filename in ('Begin.f', 'Moog.f', 'Moogsilent.f')]
+
+    # Setup: Move and create copies of the original
+    for make_file in run_make_files:
+        move(make_file, make_file + '.original')
+        copy(make_file + '.original', make_file)
+
+    for moog_file in hardcoded_moog_files:
+        move(moog_file, moog_file + '.original')
+        copy(moog_file + '.original', moog_file)
+
+    # Update the run make files with the configuration
+    for line in fileinput.input(run_make_files, inplace=True):
+        line = line.replace('#$CONFIGURATION', configuration)
+
+        sys.stdout.write(line)
+        
+    # Update the MOOG files
+    for line in fileinput.input(hardcoded_moog_files, inplace=True):
+        line = line.replace('$SRCDIR', src_dir)
+        line = line.replace('$DATADIR', data_dir)
+        line = line.replace('$MACHINE', machine)
+
+        sys.stdout.write(line)
+
+    # Run the appropriate make files
+    print('Compiling the MOOG Fortran code')
+    for make_file in run_make_files:
+        os.system('cd src;make -f %s' % make_file)
+
+    # Cleanup files: Replace with original files
+    [move(moog_file + '.original', moog_file) for moog_file in hardcoded_moog_files if os.path.exists(moog_file + '.original')]
+    [move(make_file + '.original', make_file) for make_file in run_make_files if os.path.exists(make_file + '.original')]
+
+    # Copy to bin/ directory
+    if os.path.exists('bin/')==False:
+        os.mkdir('bin/')
+    for f in ['MOOG','MOOGSILENT']:
+        if os.path.exists('src/'+f):
+            if os.path.exists('bin/'+f): os.remove('bin/'+f)
+            copy('src/'+f,'bin/'+f)
+        else:
+            print('ERROR: '+f+' NOT found')
+    
     # Get the path for the binaries
     bindir = get_bin_path()
         
     # Copy fortran binaries to bin/ directory
-    for f in ['babsma_lu','bsyn_lu']:
+    for f in ['MOOG','MOOGSILENT']:
         if os.path.exists('bin/'+f):
             if os.path.exists(bindir+f): os.remove(bindir+f)
             print('Copying bin/'+f+' -> '+bindir+'/'+f)
-            shutil.copyfile('bin/'+f,bindir+'/'+f)
+            copyfile('bin/'+f,bindir+'/'+f)
             # Make executable
             os.chmod(bindir+'/'+f,0o755)
         else:
             print('bin/'+f+' NOT FOUND')
-            
-    ## Download and convert linelists
-    #curdir = os.path.abspath(os.curdir)
-    #subprocess.check_call('make', cwd=curdir+'/linelists', shell=True)    
+
     
-class CustomInstall(install):
-    """Custom handler for the 'install' command."""
-    def run(self):
-        compile_and_install_software()
-        super().run()
+    ## Copy the AquaTerm framework
+    #if not os.path.exists('/Library/Frameworks/AquaTerm.framework/'):
+    #    try:
+    #        system_call('cp -R %s /Library/Frameworks/AquaTerm.framework/' % os.path.join(repository_dir, 'lib/AquaTerm.framework/'))
+    #    except:
+    #        sys.stdout.write("AquaTerm framework could not be installed to /Library/Frameworks/AquaTerm.framework\n")
+    #    else:
+    #        sys.stdout.write("AquaTerm framework copied to /Library/Frameworks/AquaTerm.framework\n")
+
+## Distutils setup information
+#setup(
+#    name='moogpy',
+#    version=__version__,
+#    author='David Nidever',
+#    author_email='dnidever@montana.edu',
+#    maintainer='David Nidever',
+#    maintainer_email='dnidever@montana.edu',
+#    download_url='http://github.com/dnidever/moogpy',
+#    description='Spectrum synthesis and LTE line analysis.',
+#)
 
 setup(name='moogpy',
       version='1.0.0',
@@ -93,11 +206,11 @@ setup(name='moogpy',
       author='David Nidever',
       author_email='dnidever@montana.edu',
       url='https://github.com/dnidever/moogpy',
-      #scripts=['bin/synple'],
+      #scripts=['bin/MOOG','bin/MOOGSILENT'],
       requires=['numpy','astropy(>=4.0)','scipy','matplotlib','dlnpyutils'],
-      cmdclass={'install': CustomInstall},
+      #cmdclass={'install': CustomInstall},
       zip_safe = False,
       include_package_data=True,
-      packages=find_namespace_packages(where="python"),
+      #packages=find_namespace_packages(where="python"),
       package_dir={"": "python"}   
 )
